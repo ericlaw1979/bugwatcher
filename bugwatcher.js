d88ce18fb82d2597238f1ee3ad25bd5fe51309ca
Eric Lawrence <ericlaw1979@hotmail.com>
2016-09-06 20:31:52 -0500

    Add preview of updated comments

--- bugwatcher.js
+++ bugwatcher.js
@@ -6,27 +6,50 @@ const cCommentsAtLoad = document.querySelectorAll('.issuecommentheader').length;
 let idTimer = window.setTimeout( updateClock, iSyncMS);
 let cUpdates = 0;
 
+function Now() {
+  return (new Date()).toLocaleTimeString(sLang, { hour12: false });
+}
+
 // Refetch this page from the server to see if any items have been added.
 function checkForUpdates() {
   const oReq = new XMLHttpRequest();
   oReq.addEventListener("load",  function(e) { 
     let newDoc = e.target.response;
-    let arrCommentsNow = newDoc.querySelectorAll('.issuecommentheader');
+    let arrCommentsNow = newDoc.querySelectorAll('.issuecomment');
     let cCommentsNow = arrCommentsNow.length;
 
     // Issue Unchanged?
     if (cCommentsNow == cCommentsAtLoad) return;
 
-    document.getElementById("uiSyncInfobar").innerText = "This issue has been updated; it now has " + cCommentsNow + ((cCommentsNow == 1) ? " comment": " comments") + ". Sync paused.";
+    document.getElementById("uiSyncInfobar").innerText = "This issue has been updated; it now has "
+                                    + cCommentsNow + ((cCommentsNow == 1) ? " comment": " comments") + ". Sync paused.";
     //document.getElementById("uiLastSync").classList.add('needs-sync');
     document.body.classList.add('needs-sync');
 
     let uiNewComments = document.createElement("div");
-    uiNewComments.innerText = "NEW COMMENTS GO HERE";
+    uiNewComments.classList.add('future-comments');
+    var frag = document.createDocumentFragment();
+
+    let uiFutureHeading = document.createElement('div');
+    uiFutureHeading.innerHTML = "<h2>New Comments Preview</h2>";
+    frag.appendChild(uiFutureHeading);
+    
+    // Inspired by https://gist.github.com/ebidel/3581825
+    [].forEach.call(arrCommentsNow, function(cmt, i) {
+      let txtHeader = cmt.querySelector('.issuecommentheader');
+      const iCommentNumber = /Comment ([0-9]+)/.exec(txtHeader.innerText)[1];
+      if (iCommentNumber <= cCommentsAtLoad) return;
+      let txtBody = cmt.querySelector('.issuecommentbody');
+      let divNew = document.createElement('div');
+      divNew.appendChild(txtHeader);
+      divNew.appendChild(txtBody);
+      frag.appendChild(divNew);
+    });
+    uiNewComments.appendChild(frag);
+
     document.getElementById("cursorarea").appendChild(uiNewComments);
 
     idTimer = null;
-    // TODO: Copy comments and store them away to fill in later https://gist.github.com/ebidel/3581825
     }, false);
 
     const fnErr = function() { document.getElementById("uiSyncInfobar").innerText = "Sync failed." };
@@ -40,7 +63,6 @@ function checkForUpdates() {
     oReq.setRequestHeader("X-Client", "BugWatcher Chrome Extension");
     oReq.timeout = 5000;
     oReq.send();
-//*[@id="hc1"]
 }
 
 const uiLastSync = document.createElement("th");
@@ -56,7 +78,7 @@ uiUserbar.parentNode.insertBefore(uiLastSync, uiUserbar);
 const uiAddComment = document.getElementById("addCommentTextArea");
 const uiSyncInfobar = document.createElement("div");
 uiSyncInfobar.id = "uiSyncInfobar";
-uiSyncInfobar.innerHTML = "<i>This issue last sync'd at " + oNow.toLocaleString() + " <span id=uiAgo></span> </i>";
+uiSyncInfobar.innerHTML = "<i>This issue last sync'd at <span id=uiAgo>" + Now() + "</span> </i>";
 //"<b>Warning</b>: This bug has been modified since you loaded this page. <a style='text-decoration:none' href='#'>Peek</a>";
 uiAddComment.parentNode.insertBefore(uiSyncInfobar, uiAddComment);
 uiSyncInfobar.addEventListener('click', function(e) { checkForUpdates(); }, false);
@@ -66,12 +88,15 @@ function updateClock() {
 
     if (idTimer) idTimer = window.setTimeout( updateClock, iSyncMS);
 
-    // Bail fast if hidden. 
-    // After this bug in Chrome is fixed https://bugs.chromium.org/p/chromium/issues/detail?id=532128
-    if (document.hidden) return;
+    // Bail fast if hidden. This will only work after this bug in Chrome 
+    // is fixed: https://bugs.chromium.org/p/chromium/issues/detail?id=532128
+    if (document.hidden) { 
+    // console.log("Skipping update while hidden!"); 
+      return; 
+    }
 
     let uiAgo = document.getElementById("uiAgo");
-    uiAgo.innerText = new Date() + " (" + ++cUpdates + ") ";
+    uiAgo.innerText = Now() + " (" + ++cUpdates + ") ";
 
     checkForUpdates();
 }
