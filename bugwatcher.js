f7f0c8e1fca87079ecfa982e3a54e0adbccb6c60
Eric Lawrence <ericlaw1979@hotmail.com>
2016-09-07 20:56:06 -0500

    Misc fixes

--- bugwatcher.js
+++ bugwatcher.js
@@ -1,9 +1,9 @@
 "use strict";
 
 {
-let iSyncMS = 10000;
-const cCommentsAtLoad = document.querySelectorAll('.issuecommentheader').length;
-let idTimer = window.setTimeout( updateClock, iSyncMS);
+let iSyncMS = 15000;
+let cCommentsAtLoad = document.querySelectorAll('.issuecomment').length;
+let idTimer = window.setTimeout(updateClock, iSyncMS);
 let cUpdates = 0;
 
 function Now() {
@@ -12,7 +12,7 @@ function Now() {
 
 // Refetch this page from the server to see if any items have been added.
 function checkForUpdates() {
-
+  console.log("BugWatcher: checking for updates...");
   chrome.storage.sync.get(null, function(prefs) 
   {
     if (prefs) {
@@ -20,13 +20,21 @@ function checkForUpdates() {
       if (iMS > 9999)
       {
         iSyncMS = iMS;
-        console.log("Bugwatcher: Setting interval to " + iSyncMS);
+        console.log("BugWatcher: Setting interval to " + iSyncMS);
       }
     }
   });
 
   const oReq = new XMLHttpRequest();
   oReq.addEventListener("load",  function(e) { 
+
+    // Update Top-Notice
+    uiLastSync.innerHTML = "Sync'd<br />" + oNow.toLocaleTimeString(sLang, { hour12: false });
+
+    // Update Infobar
+    let uiAgo = document.getElementById("uiAgo");
+    if (uiAgo) uiAgo.innerText = Now() + " (" + ++cUpdates + ") ";
+
     let newDoc = e.target.response;
     let arrCommentsNow = newDoc.querySelectorAll('.issuecomment');
     let cCommentsNow = arrCommentsNow.length;
@@ -39,13 +47,19 @@ function checkForUpdates() {
     //document.getElementById("uiLastSync").classList.add('needs-sync');
     document.body.classList.add('needs-sync');
 
-    let uiNewComments = document.createElement("div");
-    uiNewComments.classList.add('future-comments');
-    var frag = document.createDocumentFragment();
+    let uiNewComments = document.getElementById("uiNewComments");
+    if (!uiNewComments)
+    {
+        uiNewComments = document.createElement("div");
+        uiNewComments.id = "uiNewComments";
+        uiNewComments.classList.add('future-comments');
+        let uiFutureHeading = document.createElement('div');
+        uiFutureHeading.innerHTML = "<h2>New Comments Preview</h2>";
+        uiNewComments.appendChild(uiFutureHeading);
+        document.getElementById("cursorarea").appendChild(uiNewComments);
+    }
 
-    let uiFutureHeading = document.createElement('div');
-    uiFutureHeading.innerHTML = "<h2>New Comments Preview</h2>";
-    frag.appendChild(uiFutureHeading);
+    var frag = document.createDocumentFragment();
 
     // Inspired by https://gist.github.com/ebidel/3581825
     [].forEach.call(arrCommentsNow, function(cmt, i) {
@@ -59,8 +73,8 @@ function checkForUpdates() {
       frag.appendChild(divNew);
     });
     uiNewComments.appendChild(frag);
-
-    document.getElementById("cursorarea").appendChild(uiNewComments);
+    
+    cCommentsAtLoad = cCommentsNow;
 
     idTimer = null;
     }, false);
@@ -74,7 +88,7 @@ function checkForUpdates() {
     oReq.open("GET", document.location.href, true);         // TODO: Sanity check?
     oReq.setRequestHeader("Cache-Control", "max-age=0");
     oReq.setRequestHeader("X-Client", "BugWatcher Chrome Extension by elawrence@");
-    oReq.timeout = 5000;
+    oReq.timeout = 10000;
     oReq.send();
 }
 
@@ -85,7 +99,7 @@ const uiUserbar = document.getElementById("userbar");
 const oNow = new Date();
 const sLang = navigator.languages[0] || "en-US";
 uiLastSync.innerHTML = "Loaded<br />" + oNow.toLocaleTimeString(sLang, { hour12: false });
-uiLastSync.title = "This page was last loaded at " + oNow.toLocaleString() + "\nwith " + cCommentsAtLoad + " comments.";
+uiLastSync.title = "This page was first loaded at " + oNow.toLocaleString() + "\nwith " + cCommentsAtLoad + " comments.";
 uiUserbar.parentNode.insertBefore(uiLastSync, uiUserbar);
 
 const uiAddComment = document.getElementById("addCommentTextArea");
@@ -95,6 +109,30 @@ uiSyncInfobar.innerHTML = "<i>This issue last sync'd at <span id=uiAgo>" + Now()
 //"<b>Warning</b>: This bug has been modified since you loaded this page. <a style='text-decoration:none' href='#'>Peek</a>";
 uiAddComment.parentNode.insertBefore(uiSyncInfobar, uiAddComment);
 uiSyncInfobar.addEventListener('click', function(e) { checkForUpdates(); }, false);
+
+uiAddComment.addEventListener('paste', function(e) { 
+    // alert("pasted: " + e.clipboardData.types);
+    if (e.clipboardData.types.indexOf('Files') < 0) return;
+    var items = e.clipboardData.items;
+    for (var i = 0; i < items.length; ++i) {
+
+        if (items[i].kind == 'file' &&
+            items[i].type.indexOf('image/') !== -1) {
+
+            var blob = items[i].getAsFile();
+            window.URL = window.URL || window.webkitURL;
+            var blobUrl = window.URL.createObjectURL(blob);
+
+            var img = document.createElement('img');
+            img.src = blobUrl;
+            uiAddComment.parentNode.appendChild(img);
+        }
+        else
+            alert("Can't handle " + items[i].kind);
+    }
+
+    e.preventDefault(); // We are already handling the data from the clipboard, we do not want it inserted into the document
+}, false);
 
 
 function updateClock() {
@@ -108,15 +146,10 @@ function updateClock() {
       return; 
     }
 
-    let uiAgo = document.getElementById("uiAgo");
-    if (uiAgo) uiAgo.innerText = Now() + " (" + ++cUpdates + ") ";
-
     checkForUpdates();
 }
 
 // TODO:
-// Add "peek" option that pulls comments into markup in the right place
-// Add "Refresh keep draft comment"
 // Ping with backoff; stop pinging when user is away (somehow)
 // "Monitor" checkbox in the bug UI
 }
